665d77caf7e4afe1ce9f64794b569451a2903fd4
samjhopkins9 <samjhopkins9@gmail.com>
2023-03-15 17:22:10 -0400

    Started writing black_scholes component

---
+++ black_scholes.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+double c1(){
+    double r = 0.474;
+    
+}
+
+int main(){
+    
+}
---
+++ black_scholes.java
@@ -0,0 +1,10 @@
+public class black_scholes {
+    
+    double r0 = 0.474;
+    
+    public static void main(String[] args){
+        Quotes in = new Quotes("FAKE", "1min");
+        in.print();
+    }
+    
+}
